b8ac8d03616bab5ce2cbee0304851b6b794f070d
Aron Ambrosiani <aron@ambrosiani.se>
2018-02-10 12:52:20 +0100

    adding emojis, basic display

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import emojis from './emojis.json';
 import './App.css';
+
+const emojiList = emojis.map((emojis) => <li key={emojis.key}>{emojis.key} {emojis.translation}</li>);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">mathemoji</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <ul>{emojiList}</ul>
       </div>
     );
   }
